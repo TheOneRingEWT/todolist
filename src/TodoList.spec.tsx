6da302aa62b797ee21b792c75b5c4c0449fceb3d
rajram <rajram2000.dev@gmail.com>
2021-01-23 16:36:34 -0800

    Initial Commit

---
+++ src/TodoList.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import TodoList from './TodoList';
+
+test('renders learn react link', () => {
+  render(<TodoList />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
---
+++ src/TodoList.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import logo from './logo.svg';
+import './TodoList.scss';
+
+export const TodoList:FC = () => {
+  return (
+    <div className="TodoList">
+        <p>
+          Sample TodoList.
+        </p>
+    </div>
+  );
+}
+
+export default TodoList;
