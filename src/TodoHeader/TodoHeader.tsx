b7bd40e0b747a35fd03b056ea9b61610b044a571
rajram <rajram2000.dev@gmail.com>
2021-01-24 18:40:19 -0800

    Implement Add Todo Item

---
+++ src/TodoHeader/TodoHeader.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from "react";
+import { TodoItemData } from "../TodoItem/TodoItem";
+import "./TodoHeader.scss";
+
+export interface TodoHeaderProps {
+  onAddItemClick: OnAddItemClick;
+}
+
+export interface OnAddItemClick {
+  (addedItem: TodoItemData): void;
+}
+
+export const TodoHeader: FC<TodoHeaderProps> = (props) => {
+  const { onAddItemClick } = props;
+
+  const onAddItemClickHandler = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.stopPropagation();
+
+    onAddItemClick({ id: Date.now(), description: "", isUpdating: true });
+  };
+
+  return (
+    <div className="TodoHeader">
+      <header className="app-title">Git 'Er Done App</header>
+      <button onClick={onAddItemClickHandler}>Add Item</button>
+    </div>
+  );
+};
+
+export default TodoHeader;
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,15 +1,42 @@
-import React, { FC } from 'react';
-import logo from './logo.svg';
-import './TodoList.scss';
+import React, { FC, useState } from "react";
+import { TodoHeader } from "./TodoHeader/TodoHeader";
+import { TodoItem, TodoItemData } from "./TodoItem/TodoItem";
+import "./TodoList.scss";
 
-export const TodoList:FC = () => {
+export const TodoList: FC = () => {
+  const [todoItems, setTodoItems] = useState<TodoItemData[]>([]);
+
+  const onAddItemClickHandler = (addedItem: TodoItemData) => {
+    setTodoItems((todoItems) => [...todoItems, addedItem]);
+  };
+
+  const onDoItClickHandler = (updatedItem: TodoItemData) => {
+    setTodoItems((current) => {
+      return current.map((todo) => {
+        if (todo.id === updatedItem.id) return updatedItem;
+        return todo;
+      });
+    });
+  };
+
   return (
     <div className="TodoList">
-        <p>
-          Sample TodoList.
-        </p>
+      <TodoHeader onAddItemClick={onAddItemClickHandler} />
+      <div className="item-list">
+        {todoItems.length ? (
+          todoItems.map((todoItem) => (
+            <TodoItem
+              todoItem={todoItem}
+              onDoItClick={onDoItClickHandler}
+              key={todoItem.id}
+            />
+          ))
+        ) : (
+          <div className="add-item-prompt">Add items to get started!</div>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default TodoList;
